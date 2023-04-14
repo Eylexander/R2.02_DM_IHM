e48caf8e2a079d45ed058250ac3d2001cd26fde1
Eylexander <eylexander88@gmail.com>
2023-04-14 18:08:40 +0200

    CSS Style

--- tools/user.js
+++ tools/user.js
@@ -1,1 +1,13 @@
-kaboom
+kaboom({
+    fullscreen: false,
+    canvas: document.getElementById("monster"),
+});
+
+loadSprite("monster", "./images/favicon.ico");
+
+add([
+    sprite("monster"),
+    pos(0, 0),
+    scale(16),
+    "monster",
+]);
