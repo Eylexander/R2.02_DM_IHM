a67038358a0f1291604854812e8418aaa10cc715
Eylexander <eylexander88@gmail.com>
2023-04-24 23:48:10 +0200

    DM fini

--- tools/game.js
+++ tools/game.js
@@ -1,16 +1,189 @@
 let nom, life, money, awake;
 
+// Récupération des éléments du DOM
+let b1 = document.getElementById("b1"); // New Life
+let b2 = document.getElementById("b2"); // Run
+let b3 = document.getElementById("b3"); // Fight
+let b4 = document.getElementById("b4"); // Sleep
+let b5 = document.getElementById("b5"); // Eat
+let b6 = document.getElementById("b6"); // Show
+let b7 = document.getElementById("b7"); // Work
+let k = document.getElementById("k");   // Kill
+
+let actionbox = document.getElementById("actionbox"); // Boite d'action
+let status = document.querySelector("#status"); // Boite de status
+
+// Ajout des évènements aux boutons
+b1.addEventListener('click', newLife);
+b2.addEventListener('click', run);
+b3.addEventListener('click', fight);
+b4.addEventListener('click', sleep);
+b5.addEventListener('click', eat);
+b6.addEventListener('click', showme);
+b7.addEventListener('click', work);
+k.addEventListener('click', kill);
+
+let events = [sleep, run, fight, eat, work];
+
 // Fonction qui initialise l'etat du monstre
 function init(n, l, m) {
     nom = n;
     life = l;
     money = m;
     awake = true;
-    return showme();
 }
 
 // Fonction qui affiche les stats du monstre
 function showme() {
-    // return `Nom : ${nom}    Vie : ${life}   Argent : ${money}`;
-    return alert(`Nom : ${nom}    Vie : ${life}   Argent : ${money}`);
+    log(`Nom : ${nom}    Vie : ${life}   Argent : ${money}`);
+}
+
+function go() {
+    init(prompt("Quel est le nom de votre monstre ?"), 100, 0);
+    displayStatus(life, money, awake);
+}
+
+// window.onload = go;
+init("Dino", 100, 0);
+
+window.addEventListener('load' , () => {
+    init ("Dino", 100, 0);
+    // go();
+    displayStatus(life, money, awake);
+
+    setInterval(() => {
+        hasard();
+    }, 12000);
+});
+
+function log(message) {
+    let p = document.createElement("p");
+    p.innerHTML = message;
+    actionbox.appendChild(p);
+
+    actionbox.scrollTop = actionbox.scrollHeight;
+}
+
+function displayStatus(life, money, awake) {
+    status.childNodes[1].innerHTML = `Life : ${life}`;
+    status.childNodes[3].innerHTML = `Money : ${money}`;
+    status.childNodes[5].innerHTML = awake && life > 0 ? "Awake" : life <= 0 ? "Dead" : "Sleeping";
+
+    if (life <= 0) {
+        makeMonsterDead();
+    }
+}
+
+function run() {
+    if (!awake) {
+        log("Vous êtes endormi...");
+    } else if (life <= 0) {
+        log("Vous êtes mort...");
+    }
+    else {
+        log("Vous êtes en train de courir...");
+        if (life > 0) {
+            life -= 1;
+        }
+    }
+    displayStatus(life, money, awake);
+    
+    // Appel de la fonction makeMonsterRun de user.js
+    makeMonsterRun();
+}
+
+function fight() {
+    if (!awake) {
+        log("Vous êtes endormi...");
+    } else if (life <= 0) {
+        log("Vous êtes mort...");
+    }
+    else {
+        log("Vous êtes en train de combattre...");
+        if (life >= 3) {
+            life -= 3;
+        } else {
+            life = 0;
+        }
+    }
+    displayStatus(life, money, awake);
+
+    // Appel de la fonction makeMonsterFight de user.js
+    makeMonsterFight();
+}
+
+function work() {
+    if (!awake) {
+        log("Vous êtes endormi...");
+    } else if (life <= 0) {
+        log("Vous êtes mort...");
+    }
+    else {
+        log("Vous êtes en train de travailler...");
+        if (life > 0) {
+            life -= 1;
+        }
+        money += 2;
+    }
+    displayStatus(life, money, awake);
+}
+
+function eat() {
+    if (!awake) {
+        log("Vous êtes endormi...");
+    } else if (life <= 0) {
+        log("Vous êtes mort...");
+    } else if (money <= 3) {
+        log("Vous n'avez pas assez d'argent...");
+    }
+    else {
+        log("Vous êtes en train de manger...");
+        if (life <= 98) {
+            life += 2;
+        } else {
+            life = 100;
+        }
+        money -= 3;
+    }
+    displayStatus(life, money, awake);
+}
+
+function sleep() {
+    if (life <= 0) {
+        log("Vous êtes mort...");
+    } else if (!awake) {
+        log("Vous êtes déjà endormi...");
+    }
+    else {
+        log("Vous êtes en train de dormir...");
+        awake = false;
+        displayStatus(life, money, awake);
+        setTimeout(() => {
+            log("Vous avez bien dormi, vous avez récupéré 1 point de vie");
+            if (life < 100) {
+                life += 1;
+            }
+            awake = true;
+        }, 7000);
+    }
+    displayStatus(life, money, awake);
+
+    // Appel de la fonction makeMonsterSleep de user.js
+    makeMonsterSleep();
+}
+
+function hasard() {
+    let i = Math.floor(Math.random() * events.length);
+    events[i]();
+}
+
+function kill() {
+    life = 0;
+    awake = false;
+    displayStatus(life, money, awake);
+}
+
+function newLife() {
+    init(prompt("Quel est le nom de votre monstre ?"), 100, 0);
+    displayStatus(life, money, awake);
 }
--- tools/user.js
+++ tools/user.js
@@ -1,25 +1,35 @@
 // Variable regardant le mode de couleur de l'utilisateur
 const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+const canva = document.getElementById("monster");
 
 // Fonction qui défini tout l'avatar du monstre mais qui peut être rappelé à chaque changement de mode de couleur
 function start() {
     // Définition de la couleur de fond en fonction du mode de couleur de l'utilisateur
     const bg = mediaQuery.matches ? [22, 22, 35] : [255, 255, 255];
-    let i = 1;
 
     // Définition de la taille de la fenêtre
     kaboom({
         fullscreen: false,
-        canvas: document.getElementById("monster"),
+        canvas: canva,
         background: bg,
         scale: 2,
         width: 128,
         height: 128,
     });
 
+    // Liste des sprites du monstre
+    dinos = [
+        "dino-lime.png",
+        "dino-blue.png",
+        "dino-red.png",
+        "dino-yellow.png",
+        "dino-green.png",
+    ];
+
     // Définition de l'animation du monstre
-    loadSprite("dino", "images/dino.png", {
-        sliceX: 4,
+    // loadSprite("dino", `images/${dinos[Math.floor(Math.random() * dinos.length)]}`, {
+    loadSprite("dino", `images/dino.png`, {
+        sliceX: 9,
         anims: {
             "idle" : {
                 from: 0,
@@ -27,32 +37,87 @@ function start() {
                 speed: 3,
                 loop: true,
             },
+            "run" : {
+                from: 4,
+                to:7,
+                speed: 3,
+                loop: true,
+            },
+            "jump" : 8
         },
     });
+
+    // Définition de l'effet de shader
+    loadShader("invert", null, `
+uniform float u_time;
+
+vec4 frag(vec3 pos, vec2 uv, vec4 color, sampler2D tex) {
+	vec4 c = def_frag();
+	float t = (sin(u_time * 4.0) + 1.0) / 2.0;
+	return mix(c, vec4(1.0 - c.r, 1.0 - c.g, 1.0 - c.b, c.a), t);
+}
+`)
 
     // Définition de l'objet monstre
     const monster = add([
         sprite("dino"),
         pos(center()),
         origin("center"),
-        scale(i*4),
+        scale(4),
+        shader("invert", {
+            "u_time": time(),
+        }),
         "monster",
     ]);
 
+    monster.shader = null;
+
     // Définition de l'animation du monstre
     monster.play("idle");
+
     
     // Définition des évènements clavier
-    document.addEventListener('keydown', (e) => {
-        if (e.key === 'ArrowUp') {
-            i += 0.1;
-            monster.scaleTo(vec2(i));
-        }
-        if (e.key === 'ArrowDown') {
-            i -= 0.1;
-            monster.scaleTo(vec2(i));
-        }
+    document.addEventListener('click', () => {
+        monster.scaleTo(vec2(money / 40 + 4));
     });
+
+    // Jouer l'animation de course du monstre quand la fonction run() est appelée
+    function makeMonsterRun() {
+        monster.play("run");
+        setTimeout(() => {
+            monster.play("idle");
+        }, 3000);
+    }
+
+    // Exporter la fonction makeMonsterRun() pour pouvoir l'appeler dans game.js
+    window.makeMonsterRun = makeMonsterRun;
+
+    // Jouer l'animation de sleep du monstre quand la fonction sleep() est appelée
+    function makeMonsterSleep() {
+        monster.play("sleep");
+        setTimeout(() => {
+            monster.play("idle");
+        }, 7000);
+    }
+    window.makeMonsterSleep = makeMonsterSleep;
+
+    // Jouer l'animation d'attaque du monstre quand la fonction fight() est appelée
+    function makeMonsterFight() {
+        monster.shader = "invert";
+        monster.onUpdate(() => {
+            monster.uniform["u_time"] = time();
+        });
+        setTimeout(() => {
+            monster.shader = null;
+        }, 1500);
+    }
+    window.makeMonsterFight = makeMonsterFight;
+
+    // Jouer l'animation de mort du monstre quand la fonction kill() est appelée
+    function makeMonsterDead() {
+        monster.play("dead");
+    }
+    window.makeMonsterDead = makeMonsterDead;
 }
 
 // Fonction qui détruit tout l'avatar du monstre lors d'un changement de mode de couleur
@@ -63,3 +128,13 @@ mediaQuery.addEventListener('change', () => {
 
 // Lancement de la fonction start()
 start();
+
+// Resolution de CORS
+var ctx = canva.getContext('2d');
+
+var img = new Image();
+img.crossOrigin = "null";
+img.src = "https://eylexander.xyz/IHM/images/dino.png";
+img.onload = function() {
+    ctx.drawImage(img, 0, 0);
+}
